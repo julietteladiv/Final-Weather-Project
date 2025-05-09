dc4cb7f9b93b784dceef9849e1effd1e8ffd85f0
julietteladiv <juliettevidal01@gmail.com>
2025-05-09 12:19:03 +0100

    inout basic JS
    be careful with input type

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+let now = new Date();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let hours = now.getHours();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
 
+let currentDay = document.querySelector("#day");
+currentDay.innerHTML = day;
+let currentTime = document.querySelector("#time");
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+function changeCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-form-input").value;
+  let newCity = document.querySelector("#location");
+  newCity.innerHTML = input;
+}
+let form = document.querySelector("#form");
+form.addEventListener("submit", changeCity);
